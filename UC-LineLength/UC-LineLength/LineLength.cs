e630297ecb815e5ebd9c5e32613d34fa50bc0db1
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-16 20:48:29 +0530

    [Nantha] - commiting UC-2-Compare line

--- UC-LineLength/UC-LineLength/LineLength.cs
+++ UC-LineLength/UC-LineLength/LineLength.cs
@@ -10,7 +10,9 @@ namespace UC_LineLength
     {
         public void createLine()
         {
-            
+            double[] length = new double[2];
+            for (int i = 0; i < 2; i++)
+            {
                 Console.WriteLine("Enter value of X1");
                 int x1 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Enter value of Y2");
@@ -19,9 +21,10 @@ namespace UC_LineLength
                 int x2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Enter value of Y2");
                 int y2 = Convert.ToInt32(Console.ReadLine());
-                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-                Console.WriteLine(length);
-            
+                length[i] = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine(length[i]);
+            }
+            Console.WriteLine("Both lines with same length is " + length[1].Equals(length[0]));
         }
     }
 }
