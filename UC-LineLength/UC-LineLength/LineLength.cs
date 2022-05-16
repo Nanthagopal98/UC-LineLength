c03002116b04b6ee7ffc9780ba2b3d3274e65886
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-16 20:21:29 +0530

    [Nantha] - commiting UC-1-Create line

---
+++ UC-LineLength/UC-LineLength/LineLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_LineLength
+{
+    public class LineLength
+    {
+        public void createLine()
+        {
+            
+                Console.WriteLine("Enter value of X1");
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value of Y2");
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value of X2");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter value of Y2");
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine(length);
+            
+        }
+    }
+}
---
+++ UC-LineLength/UC-LineLength/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+UC_LineLength.LineLength run = new UC_LineLength.LineLength();
+run.createLine();
